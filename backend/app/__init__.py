22c92cf5312ee05572f25797f850d858fcaa8f50
gabiray <ciobanugabriel2020@gmail.com>
2026-04-17 00:46:11 +0300

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -18,6 +18,7 @@ def create_app():
             "http://localhost:5174",
             "http://127.0.0.1:5173",
             "http://127.0.0.1:5174",
+            "https://secure-notes-1-562b.onrender.com",
         ]}},
         supports_credentials=False,
         allow_headers=["Content-Type", "Authorization"],
